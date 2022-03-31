8b82343cdebe493b9321d6cfca5eaf59d416c774
PanchoLab <87216138+PanchoLab@users.noreply.github.com>
2022-03-31 18:30:16 -0300

    Update index.php
    otro cambio

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<title>Altos Pinos - Construcción & Corredora de Propiedades   !!!</title>
+<title>Altos Pinos - Construcción & Corredora de Propiedades   !!! !!</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <script language="JavaScript" type="text/JavaScript">
 <!--
